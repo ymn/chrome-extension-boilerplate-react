bfab688db65c61d0a2ffca23203dd8a08e260be5
Onur <onur@taskin.com>
2021-10-06 13:35:05 +0300

    Switched messagging kit to chomex

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,12 +1,13 @@
 // background.js, a background script
-import { numberStream } from './messages'
+import { Router } from 'chomex';
 
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
 
-// numberStream is an RxJs Observable
-numberStream.subscribe(([n, sender]) => {
-    console.log('the data passed to sendNumber', n)
-    // Sender is a Chrome runtime MessageSender
-    console.log('the message sender', sender)
-})
+const router = new Router();
+router.on("/users/get", (message) => {
+    console.log('', message)
+    return "mike";
+});
+
+chrome.runtime.onMessage.addListener(router.listener());
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import { sendNumber } from '../Background/messages'
+import { Client } from 'chomex';
 import './Popup.css';
 
 const Popup = () => {
+  const bgClient = new Client(chrome.runtime);
+  const [userName, setUsername] = React.useState("");
+
+  const getUsername = async () => {
+    // Gets username from background page
+    const response = await bgClient.message("/users/get", { id: 123 });
+    setUsername(response.data)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => sendNumber(69)}>Send to Backend</button>
+        <button onClick={() => getUsername()}>Get User From Service Worker</button>
+        <p>{userName}</p>
         <p>
           Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
         </p>
